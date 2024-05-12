e9099143f6a6e9076c29fcf4691340c4d1876538
Olga Ovcharenko <157131948+mazdaolga@users.noreply.github.com>
2024-05-12 17:08:14 +0300

    update task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,21 +1,23 @@
 class Storage {
-    #items;
-    constructor(items) {
-        this.#items = items;
-    }
-    getItems() {
-        return this.#items;
-    }
-    addItem(newItem) {
-        this.#items.push(newItem);
-    }
-           
-    removeItem(itemToRemove) {
-     this.#items = this.#items.filter(item => item !== itemToRemove);
+  #items;
+
+  constructor(initialItems) {
+    this.#items = initialItems;
   }
-    
-    
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter(item => item !== itemToRemove);
+  }
 }
+
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
